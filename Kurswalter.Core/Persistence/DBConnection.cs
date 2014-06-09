7a362a4daa8a29b6c28a0731e558e1bb2f81ef87
Spl1tscr33n <koelblandreas@freenet.de>
2014-06-09 06:44:35 +0200

    Database Connection methods added

--- Kurswalter.Core/Persistence/DBConnection.cs
+++ Kurswalter.Core/Persistence/DBConnection.cs
@@ -10,6 +10,39 @@ namespace KursWalter.Persistence
     class DBConnection
     {
         MySqlConnection conn = null;
+        protected string connectionString; 
+        public DBConnection(string host, string db_name, string user, string password)
+        {
+            if ((host == null) || (db_name == null) || (user == null) || (password == null))
+            {
+                throw new ArgumentNullException();
+            }
+            connectionString = "Server=" + host + ";Database=" + db_name + ";Uid=" + user + ";Pwd=" + password + ";";
+        }
 
+        public bool connect()
+        {
+            conn = new MySqlConnection(connectionString);
+            if (conn == null)
+                return false;
+            else
+            {
+                conn.Open();
+                return true;
+            }
+            try
+            {
+                Console.WriteLine("\n Mysql version: {0}", conn.ServerVersion);
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+        }
     }
 }
