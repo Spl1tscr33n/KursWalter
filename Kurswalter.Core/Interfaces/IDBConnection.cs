6e0561f8878c10c3d9dfc4b12f5e1e310b5a2aa8
Shadowanimal <Franz.Welker@gmx.de>
2014-06-11 22:47:30 +0200

    aenderungen an intercade dbcontent

--- Kurswalter.Core/Interfaces/IDBConnection.cs
+++ Kurswalter.Core/Interfaces/IDBConnection.cs
@@ -9,8 +9,9 @@ namespace Kurswalter.Core.Interfaces
 {
     public interface IDBConnection
     {
-        bool _isConnected { get; }
         //DBConnection(string host, string db_name, string user, string password);
-        public string connect();
+        string connect();
+        bool IsConnected{get;}
+        void disconnect();
     }
 }
