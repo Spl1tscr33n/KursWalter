9694283a241ec08bb42d8b702fccef924ee1d2fa
Spl1tscr33n <koelblandreas@freenet.de>
2014-06-12 11:33:26 +0200

    IModifyDatabase

---
+++ Kurswalter.Core/Interfaces/IModifyDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurswalter.Core.Interfaces
+{
+    public interface IModifyDatabase
+    {
+        string ErrorMessage;
+        bool AddUser( IPerson person);
+        bool DeleteUser( IPerson person);
+        bool ModifyUser(IPerson person);
+    }
+}
