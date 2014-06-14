a925c1a56287503c7e8af3d8503b43027fbd2017
thimmy687 <kaser23@gmx.de>
2014-06-14 08:44:38 +0200

    ADD: Composition root

--- KursWalter.WpfClient/App.xaml.cs
+++ KursWalter.WpfClient/App.xaml.cs
@@ -13,5 +13,21 @@ namespace KursWalter
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Composition root
+
+
+
+
+            var mainwindow = new MainWindow();
+
+            // show mainwindow
+
+            MainWindow = mainwindow;
+            MainWindow.Show();
+
+        }
     }
 }
