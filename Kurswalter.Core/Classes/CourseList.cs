e6cbee96ffc188117a955e76184e35f447228d05
thimmy687 <kaser23@gmx.de>
2014-06-14 16:14:10 +0200

    ADD: CourseListe class

---
+++ Kurswalter.Core/Classes/CourseList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kurswalter.Core.Interfaces;
+
+namespace Kurswalter.Core.Classes
+{
+    public class CourseList
+    {
+        private List<ICourse> _courseList = new List<ICourse>();
+
+        public List<ICourse> courseList { get; set; }
+
+        public void defaultCourseList()
+        {
+
+            var defaultcourseList = new List<ICourse>
+                                         {
+                                             //todo: ein oder zwei Kurse für testzwecke erstellen
+                                         };
+            _courseList = defaultcourseList;
+        }
+    }
+}
