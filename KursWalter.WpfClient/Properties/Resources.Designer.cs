dc4642963470a9d1940d5a332221ed001711b46f
thimmy687 <kaser23@gmx.de>
2014-06-14 14:36:07 +0200

    ADD: new logo

--- KursWalter.WpfClient/Properties/Resources.Designer.cs
+++ KursWalter.WpfClient/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace KursWalter.WpfClient.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap KursWalterLogo1 {
+            get {
+                object obj = ResourceManager.GetObject("KursWalterLogo1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
