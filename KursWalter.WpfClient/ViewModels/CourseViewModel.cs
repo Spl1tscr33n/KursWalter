9767b6e75aa440029c180044698eb1875809cc61
thimmy687 <kaser23@gmx.de>
2014-06-22 21:34:47 +0200

    ADD: CourseViewModel + Interface

---
+++ KursWalter.WpfClient/ViewModels/CourseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KursWalter.Core.Interfaces;
+
+namespace KursWalter.WpfClient.ViewModels
+{
+    public class CourseViewModel : ICourseViewModel
+    {
+        private readonly ICourse _course;
+
+        public CourseViewModel(ICourse course)
+        {
+            if (course == null) throw new ArgumentNullException("course");
+            _course = course;
+        }
+
+        public ICourse Course
+        {
+            get { return _course; }
+        }
+    }
+}
---
+++ KursWalter.WpfClient/ViewModels/ICourseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using KursWalter.Core.Interfaces;
+
+namespace KursWalter.WpfClient.ViewModels
+{
+    public interface ICourseViewModel
+    {
+        ICourse Course { get; }
+    }
+}
